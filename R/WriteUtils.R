7b1cb68e51dc8b22f27b606a216e495fcf6f89b1
Miu Shing (Matthew) Hung <140624966+hungms@users.noreply.github.com>
2024-11-01 18:05:34 +0000

    Update WriteUtils.R

--- R/WriteUtils.R
+++ R/WriteUtils.R
@@ -51,9 +51,9 @@ write_dataset <- function(parent, key, obj, scalar=FALSE) {
       dtype <- H5T_STRING$new(type="c", size=Inf)
       dtype$set_cset("UTF-8")
     }
-    else if (is.numeric(value)) {
-        dtype <- H5T_NATIVE_INTEGER
-    }
+    #else if (is.numeric(obj)) {
+    #    dtype <- H5T_NATIVE_INTEGER
+    #}
     if (scalar) {
         space <- H5S$new("scalar")
     }
@@ -152,6 +152,12 @@ write_data_frame <- function(parent, key, attr_df) {
         warning("Skip meta.data column: ", col, ", because of all values are NA.")
         next
       }
+      
+      # Check if the column is numeric and can be safely converted to integers
+      if (is.numeric(attr_df[[col]]) && all(attr_df[[col]] == as.integer(attr_df[[col]]), na.rm = TRUE)) {
+          attr_df[[col]] <- as.integer(attr_df[[col]])
+      }
+      
       # debug: remove "/" in key, it's not allow 
       col2 = gsub("/", "", col)
       write_matrix(grp, col2, attr_df[[col]])
