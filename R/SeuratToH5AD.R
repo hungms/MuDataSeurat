150148957c8183d19265f2af3e8b48aa24616875
hungms <140624966+hungms@users.noreply.github.com>
2025-07-15 14:41:11 +0100

    convert multiple reductions and update argument names

--- R/SeuratToH5AD.R
+++ R/SeuratToH5AD.R
@@ -2,16 +2,20 @@
 #'
 #' Convert Seurat to AnnData with SeuratDisk::Convert()
 #' @param obj Seurat object
-#' @param h5ad path for output .h5ad file
+#' @param assay assay name for gene expression. Defaults to "RNA"
+#' @param adt_name assay name for adt_name counts if any
 #' @param columns a vector of metadata columns to keep. Defaults to NULL to keep all columns
 #' @param pca reduction name for X_pca. Defaults to "pca"
 #' @param umap reduction name for X_umap. Defaults to "umap"
-#' @param assay assay name for gene expression. Defaults to "RNA"
-#' @param adt assay name for ADT counts if any
-#' @param return_genes if TRUE, return genes from assays named c("CC", "BCR", "TCR", "MHC") to current assay. Defaults to TRUE
+#' @param snn graph name for SNN. Defaults to "RNA_snn"
+#' @param nn graph name for NN. Defaults to "RNA_nn"
+#' @param h5ad path for output .h5ad file
 #' @import stringr
 #' @export
-SeuratToH5AD <- function(obj, h5ad, assay = "RNA", adt = NULL, columns = NULL, pca = NULL, umap = NULL, snn = NULL, nn = NULL){
+SeuratToH5AD <- function(obj, assay = "RNA", h5ad, umap, adt_name = NULL, columns = NULL, reductions = NULL, snn = NULL, nn = NULL){
+
+    # umap must be present
+    stopifnot(umap %in% names(obj@reductions))
 
     # downgrade to v4
     options(Seurat.object.assay.version = "v3")
@@ -37,13 +41,13 @@ SeuratToH5AD <- function(obj, h5ad, assay = "RNA", adt = NULL, columns = NULL, p
             obj@meta.data[[i]] <- ifelse(is.na(obj@meta.data[[i]]), "NA", obj@meta.data[[i]])
             obj@meta.data[[i]] <- as.character(obj@meta.data[[i]])}}
     
-    # add adt features to metadata
-    if(!is.null(adt)){
-        stopifnot(adt %in% names(obj@assays))
-        adt_features <- paste0(tolower(adt), "_", rownames(obj[[adt]]))
-        adt <- FetchData(obj, vars = adt_features, slot = "counts")
-        obj@meta.data[colnames(adt)] <- NULL
-        obj@meta.data <- cbind(obj@meta.data, adt)}
+    # add adt_name features to metadata
+    if(!is.null(adt_name)){
+        stopifnot(adt_name %in% names(obj@assays))
+        adt_features <- paste0(tolower(adt_name), "_", rownames(obj[[adt_name]]))
+        adt_counts <- FetchData(obj, vars = adt_features, slot = "counts")
+        obj@meta.data[colnames(adt_counts)] <- NULL
+        obj@meta.data <- cbind(obj@meta.data, adt_counts)}
 
     # convert assay to V3
     for(i in names(obj@assays)){
@@ -53,15 +57,14 @@ SeuratToH5AD <- function(obj, h5ad, assay = "RNA", adt = NULL, columns = NULL, p
     # store PCA/UMAP/neighbours
     DefaultAssay(obj) <- assay
 
-    if(!is.null(pca)){
-        stopifnot(pca %in% names(obj@reductions))
-        obj@reductions[["pca"]] <- obj@reductions[[pca]]
-        obj@reductions[["pca"]]@assay.used <- assay
-        }
-    if(!is.null(umap)){
-        stopifnot(umap %in% names(obj@reductions))
-        obj@reductions[["umap"]] <- obj@reductions[[umap]]
-        obj@reductions[["umap"]]@assay.used <- assay
+    obj@reductions[["umap"]] <- obj@reductions[[umap]]
+    obj@reductions[["umap"]]@assay.used <- assay
+
+    if(!is.null(reductions)){
+        stopifnot(all(reductions %in% names(obj@reductions)))
+        for(i in reductions){
+            obj@reductions[[i]] <- obj@reductions[[i]]
+            obj@reductions[[i]]@assay.used <- assay}
         }
     if(!is.null(snn)){
         stopifnot(snn %in% names(obj@graphs))
